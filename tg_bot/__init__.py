e9937d55a5718fe02cec9751c728322f13cabe44
Darkness Hidden <kmd36rk@gmail.com>
2020-05-14 14:01:03 +0700

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -51,11 +51,11 @@ if ENV:
     DB_URI = os.environ.get('DATABASE_URL')
     DONATION_LINK = os.environ.get('DONATION_LINK')
     LOAD = os.environ.get("LOAD", "").split()
-    NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
+    NO_LOAD = os.environ.get("NO_LOAD", "").split()
     DEL_CMDS = bool(os.environ.get('DEL_CMDS', False))
     STRICT_GBAN = bool(os.environ.get('STRICT_GBAN', False))
     WORKERS = int(os.environ.get('WORKERS', 8))
-    BAN_STICKER = os.environ.get('BAN_STICKER', 'CAADAgADOwADPPEcAXkko5EB3YGYAg')
+    BAN_STICKER = os.environ.get('BAN_STICKER', '')
     ALLOW_EXCL = os.environ.get('ALLOW_EXCL', False)
 
 else:
